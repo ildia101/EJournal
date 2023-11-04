5f00d63bfd60534dfa64c52446c10312b1a899ec
burbon-79 <ilya-ilya-2007@ukr.net>
2023-11-04 21:00:32 +0200

    Added Maven support

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,2 @@
-rm -rf "C:\Program Files (x86)\Apache Software Foundation\Tomcat 10.0\webapps\EJournal\WEB-INF\classes\org"
-cp -r "C:\Users\Illia\IdeaProjects\EJournal\out\production\EJournal\org" "C:\Program Files (x86)\Apache Software Foundation\Tomcat 10.0\webapps\EJournal\WEB-INF\classes"
-
-rm -rf "C:\Program Files (x86)\Apache Software Foundation\Tomcat 10.0\webapps\EJournal\jsp"
-cp -r "C:\Users\Illia\IdeaProjects\EJournal\jsp" "C:\Program Files (x86)\Apache Software Foundation\Tomcat 10.0\webapps\EJournal"
-
-curl --user admin:admin http://localhost:8080/manager/text/reload?path=/EJournal
+mvn clean package
+cp -r "C:\Users\Illia\IdeaProjects\EJournal\target\ejournal-1.0.war" "C:\Program Files (x86)\Apache Software Foundation\Tomcat 10.0\webapps"
